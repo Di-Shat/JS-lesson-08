a3b301dd4b59cce5d17dfafe19e638174bfa3458
Dmytro Shaturnyi <shaturnyj120293@gmail.com>
2019-03-30 16:46:02 +0200

    lesson-08

---
+++ script.js
@@ -0,0 +1,102 @@
+/*Task-01
+Напилите код, выводящий на страницу текущее время в течение 100 секунд
+
+Подсказка: создайте элемент, в котором будет отображаться текущее время
+
+Контент элемента должен обновляться каждую секунду*/
+var clock = document.body.appendChild(
+    document.createElement('h1')
+);
+clock.innerText = new Date().toLocaleString().split(', ')[1]
+for(var i = 0; i < 100; i++) {
+    (function(arg){
+        return function(){
+            setTimeout(
+                () => clock.innerText = new Date().toLocaleString().split(', ')[1],
+                1000 * arg
+            );
+        };
+    })(i)();
+};
+//////////////////////////////////////////////////////////////////////////////////////////
+/*Task-02
+Завершите код рекурсивной функции typeMessage так, чтобы при ее вызове на страницу выводился один символ в секунду
+
+var typeMessage = ( function ( velocity ) {
+    let container = document.getElementById ( "demo" ) ?
+        document.getElementById ( "demo" ) :
+        document.body.appendChild (
+            document.createElement ( "h3" )
+        )
+    container.style = `color: magenta;`
+    var index = 0
+    return function ( message ) {
+        ...
+    }
+})( 1 )
+
+typeMessage ( `Welcome to the hell` )*/
+var typeMessage = ( function ( velocity ) {
+    var container = document.getElementById ( "demo" ) ?
+        document.getElementById ( "demo" ) :
+        document.body.appendChild (
+            document.createElement ( "h3" )
+        )
+    container.style = `color: magenta;`
+    let index = 0
+  
+    return function recurs ( message ) {
+      let letter = message.split("").shift()
+      message = message.slise(1)
+      setTimeout(
+      () => conteiner.textContent += letter
+      , 1000 * index++ * velocity  
+      )
+  
+    while (message.length) recurs(message)
+    }
+})( 1 )
+
+typeMessage ( `Welcome to the hell` )
+////////////////////////////////////////////////////////////////////////////////////////////
+/*Task-03
+Нужно сделать так, чтобы у всех экзепляров, созданных конструктором User, был унаследованный метод counter(), который считает созданные конструктором экземпляры
+
+Каждый вызов конструктора увеличивает счетчик экземпляров на 1 и помещать новое значение счетчика в свойство id созданного экземпляра:
+
+function User ( name ) {
+    this.name = name
+    this.id = this.counter()
+}
+
+...
+
+var users = [
+    new User ( "Семен" ),
+    new User ( "Антон" ),
+    new User ( "Демьян" ),
+    new User ( "Василий" )
+]
+:warning: После выполнения кода:
+
+users[1].id = users[1].counter()
+свойство id экземпляра не должно измениться, так же, как и значение счетчика*/
+function User ( name ) {
+    this.name = name
+    this.id = this.counter()
+}
+
+User.prototype.counter = (function(){
+	var id = 0
+    	return function(){
+            return typeof this.id === 'number' ? id : id++
+		}
+})()
+
+var users = [
+    new User ( "Семен" ),
+    new User ( "Антон" ),
+    new User ( "Демьян" ),
+    new User ( "Василий" )
+]
+////////////////////////////////////////////////////////////////////////////////////////////////
